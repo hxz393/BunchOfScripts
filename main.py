85658f4e4e3dba6c20b7969b8737861b6dd42264
hxz393 <hxz393@gmail.com>
2023-07-02 02:35:04 +0800

    123

--- main.py
+++ main.py
@@ -11,28 +11,30 @@ if __name__ == '__main__':
     ########## 正常使用  ##############
 
     try:
-        # # 整理临时文件夹，修改文件夹名中的特殊字符。将修改后的文件夹移动到 target_path，完成后手动将文件夹从 target_path 移回 source_path。
-        # return_dict = my_scripts.rename_folder_to_common(source_path=r'B:\1.临时', target_path=r'B:\2.脚本')
-        # for k, v in return_dict.items():
-        #     print(f"{k} 移动到 {v}")
+        # 整理临时文件夹，修改文件夹名中的特殊字符。将修改后的文件夹移动到 target_path，完成后手动将文件夹从 target_path 移回 source_path。
+        # return_dict = rename_folder_to_common(source_path=r'B:\1.临时', target_path=r'B:\2.脚本')
 
-        # # 根据分隔符分割后，检查并移动重复文件夹。完成后手动将文件夹从 target_dir 移回 source_dir。
-        # return_dict = my_scripts.move_duplicates(source_path=r'B:\1.临时', target_path=r'B:\2.脚本')
-        # for k, v in return_dict.items():
-        #     print(f"{k} 移动到 {v}")
+        # 根据分隔符分割后，检查并移动重复文件夹。完成后手动将文件夹从 target_dir 移回 source_dir。
+        return_dict = move_duplicates(source_path=r'B:\1.临时', target_path=r'B:\2.脚本')
+
+        if return_dict:
+            for k, v in return_dict.items():
+                print(f"{k} 移动到 {v}")
+        else:
+            print('不需要处理')
 
         pass
     except Exception as e:
         logger.exception(e)
 
     ############## 性能分析 #########
-    来源目录 = r'B:\1.临时'
-    目标目录 = r'B:\2.脚本1'
-    要筛列表 = ['.mp3', '.flaC']
-    删除列表 = ['新建 RTF 文档.rtf', '新建... Microsoft PowerPoint Presentation.pptx', '啊123v']
-    文本文件 = r'tests/test_my_module/resources/sample_config.ini'
-    try:
-        返回 = get_file_paths(目标目录)
-        # print(返回)
-    except Exception as e:
-        logger.exception(e)
+    # 来源目录 = r'B:\1.临时'
+    # 目标目录 = r'B:\2.脚本1'
+    # 要筛列表 = ['.mp3', '.flaC']
+    # 删除列表 = ['新建 RTF 文档.rtf', '新建... Microsoft PowerPoint Presentation.pptx', '啊123v']
+    # 文本文件 = r'tests/test_my_module/resources/sample_config.ini'
+    # try:
+    #     返回 = get_file_paths(目标目录)
+    #     # print(返回)
+    # except Exception as e:
+    #     logger.exception(e)
--- my_module/others/list_to_str.py
+++ my_module/others/list_to_str.py
@@ -3,30 +3,20 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def list_to_str(a: Optional[List[Union[str, int]]]) -> Optional[str]:
+def list_to_str(my_list: Optional[List[Union[str, int]]]) -> Optional[str]:
     """
     将列表中的元素转换为字符串并连接在一起。元素之间用换行符("\n")分隔。
 
-    :type a: Optional[List[Union[str, int]]]
-    :param a: 输入的列表，其元素为字符串或整数。如果列表为空或为 None，将返回 None。
+    :type my_list: Optional[List[Union[str, int]]]
+    :param my_list: 输入的列表，其元素为字符串或整数。如果列表为空或为 None，将返回 None。
     :rtype: Optional[str]
     :return: 一个字符串，其中包含列表中所有元素的字符串表示形式，元素之间用换行符分隔，或者在列表为空或为 None 时返回 None。
     """
     try:
-        if a:
-            return "\n".join(str(element) for element in a)
+        if my_list:
+            return "\n".join(str(element) for element in my_list)
         else:
             return None
     except Exception as e:
         logger.error(f"An error occurred while converting list to string: {e}")
         return None
-
-
-if __name__ == "__main__":
-    a = [1, 'abc', 2, 'def']
-    print(list_to_str(a))
-    # 输出:
-    # 1
-    # abc
-    # 2
-    # def
