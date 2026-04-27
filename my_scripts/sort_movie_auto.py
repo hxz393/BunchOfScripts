1af9c90f004af4150dd096f87071d5bf5c6ba2a7
hxz393 <hxz393@gmail.com>
2026-04-27 12:45:27 +0800

    123

--- my_scripts/sort_movie_auto.py
+++ my_scripts/sort_movie_auto.py
@@ -3,7 +3,7 @@
 
 :author: assassing
 :contact: https://github.com/hxz393
-:copyright: Copyright 2025, hxz393. 保留所有权利。
+:copyright: Copyright 2026, hxz393. 保留所有权利。
 """
 import logging
 import os
@@ -19,255 +19,19 @@ from get_director_movies import get_tmdb_director_movies_all
 from my_module import read_file_to_list, write_list_to_file, read_json_to_dict, remove_target
 from scrapy_kpk import scrapy_kpk, scrapy_jeckett
 from sort_movie import sort_movie
-from sort_movie_director import sort_movie_director
-from sort_movie_mysql import insert_movie_wanted, query_imdb_local_director
-from sort_movie_ops import get_ids, safe_get, scan_ids, get_files_with_extensions, get_subdirs, parse_jason_file_name, delete_trash_files, check_local_torrent, move_all_files_to_root
-from sort_movie_request import get_imdb_movie_details, get_tmdb_search_response, get_tmdb_director_details, get_douban_response, get_douban_search_details, get_tmdb_movie_details
+from sort_movie_director import sort_director_auto
+from sort_movie_mysql import insert_movie_wanted
+from sort_movie_ops import get_ids, get_files_with_extensions, get_subdirs, parse_jason_file_name, delete_trash_files, check_local_torrent, move_all_files_to_root
+from sort_movie_request import get_tmdb_search_response, get_douban_response, get_douban_search_details
 from sort_ru import ru_search
 
 logger = logging.getLogger(__name__)
 
 CONFIG = read_json_to_dict('config/sort_movie.json')  # 配置文件
 
-TMDB_PERSON_URL = CONFIG['tmdb_person_url']  # tmdb 导演地址
 TMDB_MOVIE_URL = CONFIG['tmdb_movie_url']  # tmdb 电影地址
 
-IMDB_PERSON_URL = CONFIG['imdb_person_url']  # imdb 导演地址
 IMDB_MOVIE_URL = CONFIG['imdb_movie_url']  # imdb 电影地址
-
-
-def sort_director_auto(path: str, dst_path:str = 'A:\0b.导演别名') -> None:
-    """
-    自动整理导演目录，生成导演别名空文件
-
-    :param path: 导演目录
-    :param dst_path: 目标目录
-    :return: 无
-    """
-    # 查找 imdb 编号
-    result_list = [path]
-    director_main = os.path.basename(path)
-    logger.info(f"开始处理：{director_main}")
-    p = Path(path)
-    file_list = [str(item) for item in p.rglob('*')]
-    imdb_list = []
-    for file_path in file_list:
-        imdb_id = m.group(1) if (m := re.search(r'(tt\d+)', file_path)) else None
-        if imdb_id:
-            imdb_list.append(imdb_id)
-    if not imdb_list:
-        logger.error(f"目录内没有收集到 IMDB 编号: {director_main}")
-        time.sleep(0.5)
-        return
-    imdb_list = list(set(imdb_list))
-    director_ids = scan_ids(path)
-    director_info = {"country": [], "aka": []}
-
-    # 搜索 imdb，获取导演链接
-    nm_id = director_ids['imdb']
-    if not nm_id:
-        for imdb_id in imdb_list:
-            r = get_imdb_local_director(imdb_id, director_main)
-            # # 本地没搜到去线上再试试
-            # if not r:
-            #     r = get_imdb_director(imdb_id, director_main)
-            if r:
-                director_info["aka"].append(director_main)
-                result_list.append(r)
-                nm_id = m.group(1) if (m := re.search(r'(nm\d+)', r)) else None
-                break
-    else:
-        logger.info(f"IMDB 编号：{nm_id}")
-    if not nm_id:
-        logger.error(f"IMDB 电影导演不匹配或没有导演 {director_main}")
-        return
-
-    # 搜索 tmdb，获取导演链接
-    tmdb_id = director_ids['tmdb']
-    if not tmdb_id:
-        tmdb_id = get_tmdb_director(nm_id, director_main, imdb_list)
-        if tmdb_id:
-            result_list.append(tmdb_id)
-    else:
-        logger.info(f"TMDB 编号：{tmdb_id}")
-    if not tmdb_id:
-        logger.error(f"没有在 tmdb 找到导演链接 {director_main}")
-
-    # 搜索 douban，获取导演链接
-    douban_id = director_ids['douban']
-    if not douban_id:
-        douban_id = get_douban_director(nm_id)
-        if douban_id:
-            result_list.append(douban_id)
-    else:
-        logger.info(f"DOUBAN 编号：{douban_id}")
-    if not douban_id:
-        logger.error(f"没有在 douban 找到导演链接 {director_main}")
-
-    # 将结果写入到文件，执行自动抓取
-    logger.info(f"查询结果：{result_list}\n")
-    target_file = r'B:\2.脚本\!00.txt'
-    write_list_to_file(target_file, result_list)
-    get_ids(target_file)
-    done = sort_movie_director(read_file_to_list(target_file)[0], director_info)
-    if done == 2:
-        shutil.move(path, os.path.join(dst_path, director_main))
-
-
-def get_imdb_local_director(movie_id: str, director_main: str) -> Optional[str]:
-    """
-    搜索本地 imdb 库，获取导演信息
-
-    :param movie_id: imdb 编号
-    :param director_main: 导演主要名字
-    :return: 搜索结果，成功则返回导演链接，失败返回 None
-    """
-    directors = query_imdb_local_director(movie_id)
-
-    if not directors:
-        logger.error(f"IMDb 本地库没有找到导演！{movie_id} {director_main}")
-        return None
-
-    for d in directors:
-        name = d.get("director_name")
-        nm_id = d.get("director_id")
-        if name and director_main.lower() in name.lower():
-            return f"https://www.imdb.com/name/{nm_id}"
-        else:
-            logger.warning(f"没有匹配到导演，查询到导演名字：{name}")
-
-    return None
-
-
-def get_imdb_director(movie_id: str, director_main: str) -> Optional[str]:
-    """
-    搜索 imdb，获取导演信息
-
-    :param movie_id: imdb 编号
-    :param director_main: 导演主要名字
-    :return: 搜索结果，成功则返回导演链接，失败返回 None
-    """
-    m = get_imdb_movie_details(movie_id)
-    if not m:
-        logger.error(f"imdb 解析失败！{movie_id} {director_main}")
-        return
-
-    # 获取导演列表，这个结构有点特别，需要分步处理
-    principal_credits = safe_get(
-        m,
-        ["props", "pageProps", "aboveTheFoldData", "principalCreditsV2"],
-        default=[]
-    )
-    # 查找导演并提取名字
-    directors = []
-    for group in principal_credits:
-        if safe_get(group, ["grouping", "text"]) == "Director" or safe_get(group, ["grouping", "text"]) == "Directors":
-            for credit in safe_get(group, ["credits"], default=[]):
-                name = safe_get(credit, ["name", "nameText", "text"])
-                nm_id = safe_get(credit, ["name", "id"])
-                directors.append({
-                    "name": name,
-                    "link": f"{IMDB_PERSON_URL}/{nm_id}"
-                })
-
-    # 尝试匹配传入的导演主名字，匹配到了就返回
-    for d in directors:
-        if d["name"] and director_main.lower() in d["name"].lower():
-            return d["link"]
-        else:
-            logger.warning(f"没有匹配到导演，查询到导演名字：{d['name']}")
-
-    # 如果没有匹配到导演的名字返回 None
-    return None
-
-
-def get_tmdb_director_aka(tmdb_id: str, director_main: str) -> Optional[str]:
-    """获取导演详细信息，得到别名列表，然后匹配
-
-    :param tmdb_id: tmdb 编号
-    :param director_main: 导演主要名字
-    :return: 匹配，成功则返回导演链接，失败返回 None
-    """
-    p = get_tmdb_director_details(tmdb_id)
-    aka_org = list(p["also_known_as"])
-    aka_org.append(p['name'])
-    aka = [i.lower().replace(" ", "") for i in aka_org]
-    link = f"{TMDB_PERSON_URL}/{tmdb_id}"
-    if director_main.lower().replace(" ", "") in aka:
-        return link
-    else:
-        logger.warning(f"没有匹配到导演，查询到导演名字：{aka_org} {link}")
-        return None
-
-
-def get_tmdb_director(nm_id: str, director_main: str, imdb_list: list) -> Optional[str]:
-    """
-    两种方式搜索 tmdb，获取导演信息
-
-    :param nm_id: nm 导演编号
-    :param director_main: 导演主要名字
-    :param imdb_list: 通过 imdb 列表搜索电影获取导演信息，备用方式
-    :return: 搜索结果，成功则返回导演链接，失败返回 None
-    """
-    # 搜索导演，获取导演信息
-    data = get_tmdb_search_response(nm_id)
-    if not data:
-        return
-
-    persons = data.get("person_results", [])
-    if persons:
-        # 一般只有一个结果
-        person = persons[0]
-        tmdb_id = person.get('id')
-        return f"{TMDB_PERSON_URL}/{tmdb_id}"
-    else:
-        logger.warning(f"没有在 TMDB 上搜索到导演：{nm_id}，尝试通过电影获取导演")
-
-    # 麻烦的方法
-    for imdb in imdb_list:
-        r = get_tmdb_search_response(imdb)
-        if r:
-            movie_results = r.get('movie_results', [])
-            if not movie_results:
-                continue
-            movie_id = movie_results[0].get('id')
-            movie_details = get_tmdb_movie_details(movie_id)
-            crew_list = movie_details['casts'].get('crew', [])
-            # 在 crew 中筛选出 job == 'Director' 的人员
-            directors = []
-            for member in crew_list:
-                if member.get('job') == 'Director':
-                    member_id = member.get('id')
-                    directors.append({
-                        "name_id": member_id,
-                        "name": member.get('name'),
-                        "link": f"{TMDB_PERSON_URL}/{member_id}"
-                    })
-            # 尝试匹配 director_main
-            for d in directors:
-                if d["name"] and d["name"].lower() == director_main.lower():
-                    return d["link"]
-                else:
-                    result = get_tmdb_director_aka(d["name_id"], director_main)
-                    if result:
-                        return result
-    return
-
-
-def get_douban_director(nm_id: str) -> Optional[str]:
-    """
-    搜索 douban，获取导演信息
-
-    :param nm_id: imdb 导演编号
-    :return: 搜索结果，成功则返回导演链接，失败返回 None
-    """
-    # 搜索编号，获取结果
-    r = get_douban_response(nm_id, "director_search")
-    if not r:
-        logger.error("没有获取到豆瓣搜索响应")
-        return
-
-    return get_douban_search_details(r)
 
 
 def sort_movie_auto(path: str, target_file: str) -> None:
@@ -479,7 +243,7 @@ def achieve_director(path: str) -> None:
     logger.info(f"处理导演归档：{director_name}")
     time.sleep(0.1)
     # 获取没有资源的电影，存到 wanted 表中
-    result = get_tmdb_director_movies_all(path, pass_exists=True)
+    result = get_tmdb_director_movies_all(path, skip_existing=True)
     if not result:
         return
     logger.info(f"缺少电影列表：{result}")
