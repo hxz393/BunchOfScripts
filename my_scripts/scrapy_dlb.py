d44314137385b1593c0a66687d37083c2fadb621
hxz393 <hxz393@gmail.com>
2026-04-27 09:37:27 +0800

    123

--- my_scripts/scrapy_dlb.py
+++ my_scripts/scrapy_dlb.py
@@ -3,13 +3,12 @@
 
 :author: assassing
 :contact: https://github.com/hxz393
-:copyright: Copyright 2026, hxz393. 保留所有权利。
+:copyright: Copyright 2027, hxz393. 保留所有权利。
 """
 import logging
 import os
 import re
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from typing import List, Dict
 
 import requests
 from bs4 import BeautifulSoup
@@ -17,12 +16,7 @@ from requests.adapters import HTTPAdapter
 from retrying import retry
 from urllib3.util.retry import Retry
 
-import requests
-from bs4 import BeautifulSoup
-from retrying import retry
-
 from my_module import normalize_release_title_for_filename, read_json_to_dict, sanitize_filename, write_list_to_file
-
 
 CONFIG_PATH = 'config/scrapy_dlb.json'
 CONFIG = read_json_to_dict(CONFIG_PATH)  # 配置文件
@@ -30,9 +24,8 @@ DLB_COOKIE = CONFIG['dlb_cookie']  # 用户甜甜
 REQUEST_HEAD = CONFIG['request_head']  # 请求头
 REQUEST_HEAD["Cookie"] = DLB_COOKIE  # 请求头加入认证
 
-DLB_URL = CONFIG['dlb_url']  # hde 地址
+DLB_URL = CONFIG['dlb_url']  # dlb 地址
 OUTPUT_DIR = CONFIG['output_dir']  # 输出目录
-
 
 logger = logging.getLogger(__name__)
 requests.packages.urllib3.disable_warnings()
@@ -62,11 +55,6 @@ def scrapy_dlb(start_page: int = 1, end_title="Tawai.A.voice.from.the.forest.201
         result_list = parse_dlb_response(response)
         logger.info(f"共 {len(result_list)} 个结果")
 
-        # 循环抓取
-        # for list_item in result_list:
-        #     visit_dlb_url(list_item)
-        # return
-
         process_all(result_list, max_workers=20)
 
         # 检查日期
@@ -74,13 +62,11 @@ def scrapy_dlb(start_page: int = 1, end_title="Tawai.A.voice.from.the.forest.201
             logger.info("没有新发布，完成")
             break
 
-        # logger.info(f"结果：{result_list}")
         logger.warning("-" * 255)
         start_page += 1
 
 
-def process_all(result_list, max_workers=5):
-    results = []
+def process_all(result_list, max_workers=5) -> None:
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         # 提交所有任务
         future_to_item = {
@@ -91,12 +77,9 @@ def process_all(result_list, max_workers=5):
         for future in as_completed(future_to_item):
             item = future_to_item[future]
             try:
-                ret = future.result()
+                future.result()
             except Exception as exc:
                 logger.error(f"[ERROR] {item} -> {exc!r}")
-            else:
-                results.append(ret)
-    return results
 
 
 @retry(stop_max_attempt_number=15, wait_random_min=15000, wait_random_max=20000)
@@ -104,7 +87,6 @@ def get_dlb_response(url: str) -> requests.Response:
     """请求流程"""
     logger.info(f"访问 {url}")
     response = session.get(url, timeout=30, verify=False, headers=REQUEST_HEAD)
-    # response.encoding = 'utf-8'
     if response.status_code != 200:
         logger.error(f"请求失败，重试 {response.status_code}：{url}")
         raise Exception(f"请求失败")
@@ -170,8 +152,6 @@ def visit_dlb_url(result_item: dict):
                 imdb_id = m2.group(1)
                 break
 
-    # 存回 result_item 或返回
-    result_item["imdb"] = imdb_id
     file_name = normalize_release_title_for_filename(result_item['title'])
     file_name = sanitize_filename(file_name)
     file_name = f"{file_name} ({result_item['size']})[{imdb_id}].dlb"
